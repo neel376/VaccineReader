f3646b47718cb07301925bf726201200c554b909
Neel Singh <neelsingh2012@gmail.com>
2018-05-12 13:50:51 -0400

    added basic functionality

---
+++ VaccineReader/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace VaccineReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataBox = new System.Windows.Forms.RichTextBox();
+            this.submit = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dataBox
+            // 
+            this.dataBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataBox.Location = new System.Drawing.Point(12, 12);
+            this.dataBox.Name = "dataBox";
+            this.dataBox.Size = new System.Drawing.Size(787, 364);
+            this.dataBox.TabIndex = 0;
+            this.dataBox.Text = "";
+            this.dataBox.TextChanged += new System.EventHandler(this.dataBox_TextChanged);
+            // 
+            // submit
+            // 
+            this.submit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.submit.Location = new System.Drawing.Point(713, 398);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(86, 31);
+            this.submit.TabIndex = 1;
+            this.submit.Text = "Submit";
+            this.submit.UseVisualStyleBackColor = true;
+            this.submit.Click += new System.EventHandler(this.submit_Click);
+            // 
+            // clear
+            // 
+            this.clear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.clear.Location = new System.Drawing.Point(598, 398);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(87, 31);
+            this.clear.TabIndex = 2;
+            this.clear.Text = "Clear";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(843, 441);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.submit);
+            this.Controls.Add(this.dataBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox dataBox;
+        private System.Windows.Forms.Button submit;
+        private System.Windows.Forms.Button clear;
+    }
+}
+
---
+++ VaccineReader/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VaccineReader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] removeExtraCharacters(string[] data)
+        {
+            string[] fixedData = { data[0].Substring(3), data[1].Substring(3), data[2].Substring(3) };
+            return fixedData;
+        }
+        
+        string[] parseLicense(string rawData)
+        {
+            string[] rawDataArr = rawData.Split('\n');
+            
+            // return error if scan did not return at least 10 lines
+            if (rawDataArr.Length < 10)
+            {
+                string[] err = { "Error" };
+                return err;
+
+            }
+            string[] data = { rawDataArr[6], rawDataArr[7], rawDataArr[10] }; // assign lastName, firstName, DOB to data array
+            data = removeExtraCharacters(data); //remove extra characters at begginning of string
+            return data;
+        }
+
+        private void submit_Click(object sender, EventArgs e)
+        {
+            string rawData = "";
+            string[] patientData = new string[3];
+            
+            //parse raw data
+            rawData = this.dataBox.Text;
+            patientData = parseLicense(rawData);
+            
+            // Output an error if scan didn't work properly
+            if (patientData[0].Equals("Error")) MessageBox.Show("Error: Scan did not return proper values");
+            
+            // Scan worked so display the data
+            else MessageBox.Show("Last Name: " + patientData[0] + " First Name: " + patientData[1] + " DOB: " + patientData[2]);    
+        }
+
+        private void dataBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            this.dataBox.Text = String.Empty;
+        }
+    }
+}
